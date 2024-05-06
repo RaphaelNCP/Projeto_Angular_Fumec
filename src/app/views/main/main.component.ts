b1bf144721db9c8a2822e59c0bc129f59fe964c2
Vinicius Dias <diasvinicius95@outlook.com>
2024-05-06 19:20:46 -0300

    adicionando projeto atualizado

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-content',
+  imports: [RouterOutlet],
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
   standalone: true,
