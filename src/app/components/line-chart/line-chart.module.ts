bd0a9e2ceecf5e7fbb646b998d9571b553cc5732
Vinicius Dias <diasvinicius95@outlook.com>
2024-05-06 14:09:24 -0300

    Testes relacionados a adicao dos graficos

---
+++ src/app/components/line-chart/line-chart.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { LineChartComponent } from './line-chart.component';
+
+@NgModule({
+  declarations: [LineChartComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [LineChartComponent],
+})
+export class AppModule {}
